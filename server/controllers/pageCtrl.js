9dc0b180e9c1c0c31daabd5ef1ce39772a6cb1c1
Cameron Miner <cameronsminer@gmail.com>
2022-11-03 14:38:14 -0600

    Setup file to work with rollbar

--- server/controllers/pageCtrl.js
+++ server/controllers/pageCtrl.js
@@ -3,5 +3,13 @@ const path = require('path')
 module.exports = {
     home: (req, res) => {
         res.sendFile(path.join(__dirname, "../../index.html"))
+    },
+
+    about: (req, res) => {
+        try {
+            doesNotExistFunc()
+        } catch {
+            rollbar.critical('Backend error with test func')
+        }
     }
 }
--- server/server.js
+++ server/server.js
@@ -3,13 +3,23 @@ require("dotenv").config()
 
 const app = express()
 
+
 app.use(express.json())
+
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: process.env.ROLLBAR_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
 
 const { home } = require('./controllers/pageCtrl')
 
 app.get("/", home)
-
+app.get("/about", about)
 
 
 
