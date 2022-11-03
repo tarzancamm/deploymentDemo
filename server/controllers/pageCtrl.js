32de3f9d4037fcd9e000415f7c4c36ac08964a18
Cameron Miner <cameronsminer@gmail.com>
2022-11-03 14:45:45 -0600

    .

--- server/controllers/pageCtrl.js
+++ server/controllers/pageCtrl.js
@@ -11,13 +11,13 @@ var rollbar = new Rollbar({
 module.exports = {
     home: (req, res) => {
         res.sendFile(path.join(__dirname, "../../index.html"))
-    },
-
-    about: (req, res) => {
-        try {
-            doesNotExistFunc()
-        } catch {
-            rollbar.critical('Backend error with test func')
-        }
     }
+
+    // about: (req, res) => {
+    //     try {
+    //         doesNotExistFunc()
+    //     } catch {
+    //         rollbar.critical('Backend error with test func')
+    //     }
+    // }
 }
